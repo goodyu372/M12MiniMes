68a204f6716dca8bc5a68db1799a405aeabe7e94
GoodLuck617 <826353165@qq.com>
2022-04-08 21:06:03 +0800

    添加点击按钮提示框

--- M12MinMes.MachineStatus/MachineStatusSetting.cs
+++ M12MinMes.MachineStatus/MachineStatusSetting.cs
@@ -32,54 +32,61 @@ namespace M12MinMes.MachineStatus
         private void AddMachineStatus()//添加设备状态种类
         {
             //string MachineStatusID = MachineStatusID_textBox.Text.Trim();
-            bool bIsInt1 = int.TryParse(MachineStatusID_textBox.Text.Trim().ToString(), out int MachineStatusID);
-            if (!bIsInt1)
+            DialogResult resault = MessageBox.Show("确定要创建此设备ID吗？", "确定", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (resault == DialogResult.OK)
             {
-                MessageBox.Show("设备状态ID需填整数！");
-                return;
+                bool bIsInt1 = int.TryParse(MachineStatusID_textBox.Text.Trim().ToString(), out int MachineStatusID);
+                if (!bIsInt1)
+                {
+                    MessageBox.Show("设备状态ID需填整数！");
+                    return;
+                }
+                string MachineStatusName = MachineStatusName_textBox.Text.Trim();
+                if (string.IsNullOrEmpty(MachineStatusName))
+                {
+                    MessageBox.Show("设备状态名称不能为空！");
+                    return;
+                }
+                if (MachineStatus.machineStatus.DicMachineStatus.Keys.Contains(MachineStatusID))
+                {
+                    MessageBox.Show("已经存在此设备状态ID，请重写！");
+                    return;
+                }
+                MachineStatus.machineStatus.DicMachineStatus.Add(MachineStatusID, MachineStatusName);
             }
-            string MachineStatusName = MachineStatusName_textBox.Text.Trim();
-            if(string.IsNullOrEmpty(MachineStatusName))
-            {
-                MessageBox.Show("设备状态名称不能为空！");
-                return;
-            }
-            if(MachineStatus.machineStatus.DicMachineStatus.Keys.Contains(MachineStatusID))
-            {
-                MessageBox.Show("已经存在此设备状态ID，请重写！");
-                return;
-            }
-            MachineStatus.machineStatus.DicMachineStatus.Add(MachineStatusID, MachineStatusName);
         }
 
         private void UpdateMachineStatusbutton_Click(object sender, EventArgs e)//修改设备状态种类
         {
-            bool bIsInt1 = int.TryParse(MachineStatusID_textBox.Text.Trim().ToString(), out int MachineStatusID);
-            if (!bIsInt1)
+            DialogResult resault = MessageBox.Show("确定要修改此设备状态ID吗？", "确定", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (resault == DialogResult.OK)
             {
-                MessageBox.Show("设备状态ID需填整数！");
-                return;
-            }
-            string MachineStatusName = MachineStatusName_textBox.Text.Trim();
-            if (string.IsNullOrEmpty(MachineStatusName))
-            {
-                MessageBox.Show("设备状态名称不能为空！");
-                return;
-            }
-            if (MachineStatus.machineStatus.DicMachineStatus.Keys.Contains(MachineStatusID))
-            {
-                
-                //return;
-                MachineStatus.machineStatus.DicMachineStatus.Remove(MachineStatusID);
-                MachineStatus.machineStatus.DicMachineStatus.Add(MachineStatusID, MachineStatusName);
-                MessageBox.Show("修改成功！");
-            }
-            else
-            {
-                MessageBox.Show("不存在此设备状态ID，请重新选择或移除对应的设备状态ID后再点击“创建”！");
-            }
+                bool bIsInt1 = int.TryParse(MachineStatusID_textBox.Text.Trim().ToString(), out int MachineStatusID);
+                if (!bIsInt1)
+                {
+                    MessageBox.Show("设备状态ID需填整数！");
+                    return;
+                }
+                string MachineStatusName = MachineStatusName_textBox.Text.Trim();
+                if (string.IsNullOrEmpty(MachineStatusName))
+                {
+                    MessageBox.Show("设备状态名称不能为空！");
+                    return;
+                }
+                if (MachineStatus.machineStatus.DicMachineStatus.Keys.Contains(MachineStatusID))
+                {
 
-
+                    //return;
+                    MachineStatus.machineStatus.DicMachineStatus.Remove(MachineStatusID);
+                    MachineStatus.machineStatus.DicMachineStatus.Add(MachineStatusID, MachineStatusName);
+                    MessageBox.Show("修改成功！");
+                }
+                else
+                {
+                    MessageBox.Show("不存在此设备状态ID，请重新选择或移除对应的设备状态ID后再点击“创建”！");
+                }
+            }
+            
         }
 
         private void refresh_button_Click(object sender, EventArgs e)//更新设备状态种类到表格中
@@ -133,22 +140,31 @@ namespace M12MinMes.MachineStatus
 
         private void DeleteMachineStatusbutton_Click(object sender, EventArgs e)
         {
-            bool bIsInt1 = int.TryParse(MachineStatusID_textBox.Text.Trim().ToString(), out int MachineStatusID);
-            if (MachineStatus.machineStatus.DicMachineStatus.Keys.Contains(MachineStatusID))
+            DialogResult resault = MessageBox.Show("确定要删除此设备状态ID吗？", "确定", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (resault == DialogResult.OK)
             {
-                MachineStatus.machineStatus.DicMachineStatus.Remove(MachineStatusID);
-              
-                MessageBox.Show("删除成功！");
-            }
-            else
-            {
-                MessageBox.Show("不存在此设备状态ID，删除失败！");
+                bool bIsInt1 = int.TryParse(MachineStatusID_textBox.Text.Trim().ToString(), out int MachineStatusID);
+
+                if (MachineStatus.machineStatus.DicMachineStatus.Keys.Contains(MachineStatusID))
+                {
+                    MachineStatus.machineStatus.DicMachineStatus.Remove(MachineStatusID);
+
+                    MessageBox.Show("删除成功！");
+                }
+                else
+                {
+                    MessageBox.Show("不存在此设备状态ID，删除失败！");
+                }
             }
         }
 
         private void CreatAlarm_button_Click(object sender, EventArgs e)
         {
-            AddAlarmID();
+            DialogResult resault = MessageBox.Show("确定要创建报警内容吗？", "确定", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (resault == DialogResult.OK)
+            {
+                AddAlarmID();
+            }
         }
 
         private void AddAlarmID()//添加报警内容
@@ -180,16 +196,20 @@ namespace M12MinMes.MachineStatus
 
         private void DeleteAlarm_button_Click(object sender, EventArgs e)
         {
-            bool bIsInt1 = int.TryParse(AlarmID_textBox.Text.Trim().ToString(), out int AlarmID);
-            if (MachineStatus.machineStatus.DicMachineAlarmInformation.Keys.Contains(AlarmID))
+            DialogResult resault = MessageBox.Show("确定要删除此报警内容吗？", "确定", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (resault == DialogResult.OK)
             {
-                MachineStatus.machineStatus.DicMachineAlarmInformation.Remove(AlarmID);
+                bool bIsInt1 = int.TryParse(AlarmID_textBox.Text.Trim().ToString(), out int AlarmID);
+                if (MachineStatus.machineStatus.DicMachineAlarmInformation.Keys.Contains(AlarmID))
+                {
+                    MachineStatus.machineStatus.DicMachineAlarmInformation.Remove(AlarmID);
 
-                MessageBox.Show("删除成功！");
-            }
-            else
-            {
-                MessageBox.Show("不存在此报警代码，删除失败！");
+                    MessageBox.Show("删除成功！");
+                }
+                else
+                {
+                    MessageBox.Show("不存在此报警代码，删除失败！");
+                }
             }
         }
 
@@ -223,29 +243,33 @@ namespace M12MinMes.MachineStatus
 
         private void UpdateAlarm_button_Click(object sender, EventArgs e)
         {
-            bool bIsInt1 = int.TryParse(AlarmID_textBox.Text.Trim().ToString(), out int AlarmID);
-            if (!bIsInt1)
+            DialogResult resault = MessageBox.Show("确定要修改此报警代码吗？", "确定", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (resault == DialogResult.OK)
             {
-                MessageBox.Show("报警代码，需填整数！");
-                return;
-            }
-            string AlarmName = AlarmName_textBox.Text.Trim();
-            if (string.IsNullOrEmpty(AlarmName))
-            {
-                MessageBox.Show("报警内容，不能为空！");
-                return;
-            }
-            if (MachineStatus.machineStatus.DicMachineStatus.Keys.Contains(AlarmID))
-            {
+                bool bIsInt1 = int.TryParse(AlarmID_textBox.Text.Trim().ToString(), out int AlarmID);
+                if (!bIsInt1)
+                {
+                    MessageBox.Show("报警代码，需填整数！");
+                    return;
+                }
+                string AlarmName = AlarmName_textBox.Text.Trim();
+                if (string.IsNullOrEmpty(AlarmName))
+                {
+                    MessageBox.Show("报警内容，不能为空！");
+                    return;
+                }
+                if (MachineStatus.machineStatus.DicMachineStatus.Keys.Contains(AlarmID))
+                {
 
-                //return;
-                MachineStatus.machineStatus.DicMachineStatus.Remove(AlarmID);
-                MachineStatus.machineStatus.DicMachineStatus.Add(AlarmID, AlarmName);
-                MessageBox.Show("修改成功！");
-            }
-            else
-            {
-                MessageBox.Show("不存在此报警代码，请重新选择或移除对应的报警代码后再点击“创建”！");
+                    //return;
+                    MachineStatus.machineStatus.DicMachineStatus.Remove(AlarmID);
+                    MachineStatus.machineStatus.DicMachineStatus.Add(AlarmID, AlarmName);
+                    MessageBox.Show("修改成功！");
+                }
+                else
+                {
+                    MessageBox.Show("不存在此报警代码，请重新选择或移除对应的报警代码后再点击“创建”！");
+                }
             }
         }
 
